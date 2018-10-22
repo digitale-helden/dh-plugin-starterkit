3b9c01d5de4dfde91f56485350e99a628775e4cb
setcookie <set@cooki.me>
2018-10-22 11:00:15 +0200

    minor fix

--- src/files/version.sh
+++ src/files/version.sh
@@ -9,7 +9,7 @@ then
     exit 1
 fi;
 
-PLUGIN='@pluginFileName@.php'
+PLUGIN='dh-plugin-@pluginFileName@.php'
 VERSION=$(echo $(git describe --tags) | sed -n 's/.*\([0-9]\.[0-9]\.[0-9]*\).*/\1/p')
 
 sed -i'' -e 's/\(Version\:*\).*$/\1 '$VERSION'/g' $PLUGIN
